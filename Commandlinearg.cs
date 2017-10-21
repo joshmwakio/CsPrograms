f678d923fe57b1717cbcb01bdef34697f109b918
joshmwakio <joshua.mwakio@gmail.com>
2017-10-21 15:49:49 +0300

    Add files via upload

---
+++ Commandlinearg.cs
@@ -0,0 +1,12 @@
+using System;
+namespace MyProgram
+{
+class CommandLine
+{
+      static void Main(string [] args)
+      {
+      Console.WriteLine("Hello, {0} {1} !", args[0], args[1]);
+      Console.WriteLine("Welcome to the CSharp Programming!");
+      }
+}
+}
